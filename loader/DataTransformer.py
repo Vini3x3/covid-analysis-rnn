3ae165c55189b1646f83eefb625d96f0a4786e1f
Vini3x3 <vincent.hanghang@gmail.com>
2024-08-10 14:45:42 +0800

    add loader and uniit test

---
+++ loader/DataTransformer.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def delay_list(sequences: np.ndarray, period: int) -> np.ndarray:
+    """
+    Transform 2D sequence to a list of 2D delayed list
+    :param sequences: input 2D sequence
+    :param period: length of sub-sequence
+    :return:
+    """
+    if sequences.shape[0] < period:
+        raise LookupError("invalid period size")
+    return np.array([sequences[i:i + period] for i in range(len(sequences) - period + 1)])
+
+
---
+++ unit.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from loader.DataTransformer import delay_list
+
+
+def t1_delay_happy():
+    input_arr = np.array([ [1,2,3], [4,5,6], [7,8,9], [10,11,12], [13,14,15] ])
+    output_arr = delay_list(input_arr, 2)
+    assert output_arr.shape == (4,2,3)
+    assert (output_arr[0][0] == [1,2,3]).all()
+    assert (output_arr[-1][-1] == [13, 14, 15]).all()
+
+if __name__ == '__main__':
+    t1_delay_happy()
