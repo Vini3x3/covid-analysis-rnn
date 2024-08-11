305006e7fa14beec0b36679d8264404c6746e568
Vini3x3 <vincent.hanghang@gmail.com>
2024-08-11 18:06:15 +0800

    add sample: improve verify case by using unseen data

--- rnn_simple_dot_product.py
+++ rnn_simple_dot_product.py
@@ -34,7 +34,7 @@ model = LstmModel(input_dim, hidden_dim, num_layers, output_dim)
 # train
 num_epochs = 1_000
 loss_fn = torch.nn.MSELoss()
-optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+optimiser = torch.optim.Adam(model.parameters(), lr=0.007)
 model.train()
 for epoch in range(1, num_epochs + 1):
     y_pred = model(X_train)
@@ -56,8 +56,24 @@ print("sample prediction:  ", y_pred[:5])
 y_train_sample = y_train[:5].detach().numpy().reshape(-1)
 print("sample true result: ", y_train_sample)
 
-# verify
 y_pred_round = [round(p, 1) for p in y_pred]
 y_train_round = [round(p, 1) for p in y_train_sample]
-
 assert (y_pred_round == y_train_round)
+
+# verify
+sample_x1 = sample_x2 = [0.1, 0.3, 0.4, 0.5, 0.7]
+sample_answer = 1
+
+sample_input = np.stack((x1, x2), axis=2)
+sample_input = torch.from_numpy(sample_input).type(torch.Tensor)
+
+sample_pred = model(sample_input)
+
+sample_pred = sample_pred.detach().numpy()  # revert from tensor
+sample_pred = sample_pred.reshape(-1)  # reshape back to normal list
+sample_pred = sample_pred[0]  # extract only element
+
+print("verify input: ", sample_x1, sample_x2)
+print("verify prediction:  ", sample_pred)
+print("verify true result: ", sample_answer)
+assert (round(sample_pred) == sample_answer)
