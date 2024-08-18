b49368c19b1783facf131b9e0a453a2c18bb35b2
Vini3x3 <vincent.hanghang@gmail.com>
2024-08-19 00:20:56 +0800

    add all sequence notebook

---
+++ rnn_medium_all_transform.py
@@ -0,0 +1,73 @@
+import numpy as np
+import torch
+
+from loader.DataLoader import read_sequence, read_dataframe
+from loader.DataTransformer import lag_list, moving_average
+from model.LstmModel import LstmModel
+
+# script parameter
+# MODE: MA (moving average), D1(lag 1 degree), DMA(decaying moving average) or default no change
+MODE = 'MA'
+
+
+# prepare data
+def transform_sequence(input_sequence: np.ndarray, mode: str = '') -> np.ndarray:
+    if mode == 'MA':
+        return moving_average(input_sequence, 14)
+    elif mode == 'DMA':
+        return moving_average(input_sequence, 14, 0.95)
+    elif mode == 'D1':
+        return np.diff(input_sequence)
+    else:
+        return input_sequence
+
+
+sequence = read_dataframe('all').to_numpy()
+# sequence = transform_sequence(sequence, MODE)
+shifted_sequence = lag_list(sequence, 16)  # shift into delayed sequences
+
+x_train = shifted_sequence[:, :-1, 1:]  # for each delayed sequence, take all elements except last element
+y_train = shifted_sequence[:, -1, -1]  # for each delayed sequence, only take the last element
+
+x_train = torch.from_numpy(x_train.astype('float64')).type(torch.Tensor)  # convert to tensor
+y_train = torch.from_numpy(y_train.astype('int32')).type(torch.Tensor)  # convert to tensor
+
+# build model
+input_dim = x_train.shape[-1]
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+
+model = LstmModel(input_dim, hidden_dim, num_layers, output_dim)
+
+# train
+num_epochs = 3_000
+loss_fn = torch.nn.MSELoss()
+optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+model.train()
+for epoch in range(1, num_epochs + 1):
+    y_pred = model(x_train)
+    y_pred = y_pred.reshape(-1)
+    loss = loss_fn(y_pred, y_train)
+    if epoch % 100 == 0:
+        print("Epoch: %d | MSE: %.2E" % (epoch, loss.item()))
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+# test
+model.eval()
+y_pred = model(x_train[:5])
+y_pred = y_pred.reshape(-1)
+y_pred = y_pred.detach().numpy()  # revert from tensor
+y_pred = y_pred.reshape(-1)  # reshape back to normal list
+print("sample prediction:  ", y_pred)
+
+y_train_sample = y_train[:5].detach().numpy().reshape(-1)
+print("sample true result: ", y_train_sample)
+
+# verify
+y_pred_round = [round(p) for p in y_pred]
+y_train_round = [round(p) for p in y_train_sample]
+
+assert (y_pred_round == y_train_round)
