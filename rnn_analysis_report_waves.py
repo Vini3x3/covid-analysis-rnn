7a651daa5421d89d9627c49309f2cf7b13098a08
Vini3x3 <vincent.hanghang@gmail.com>
2024-11-09 11:16:17 +0800

    feat: mount file location

--- rnn_analysis_report_waves.py
+++ rnn_analysis_report_waves.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 import torch
@@ -5,30 +7,34 @@ import torch
 from loader.DataLoader import read_dataframe
 from model.ScalingResidualLstmModel import ScalingResidualLstmModel
 
-WAVE = 4
-CHECKPOINT_DIR = 'checkpoint_wave' + str(WAVE)
+WAVE = 1
 
 
 def create_pd_top(names, data):
     return pd.DataFrame({'name': names, 'data': data}).sort_values(by='data', ascending=False).head(10)
 
+def get_file_path(wave, filename):
+    curr_dir = os.getcwd()
+    project_dir = curr_dir.split('GitHub')[0]
+    analysis_on_covid_dir = os.path.join(project_dir, 'GitHub', 'analysis-on-covid')
+    return analysis_on_covid_dir + 'checkpoint_wave' + str(wave) + '/' + filename
 
-best_train_loss = np.load(CHECKPOINT_DIR + '/best_train_loss.npy')
+best_train_loss = np.load(get_file_path(WAVE, 'best_train_loss.npy'))
 threshold = np.percentile(best_train_loss, 25)
 indices = [i for i, x in enumerate(best_train_loss) if x <= threshold]
 
-best_lstm_lag = torch.load(CHECKPOINT_DIR + '/best_lstm_lag.pt')
+best_lstm_lag = torch.load(get_file_path(WAVE, 'best_lstm_lag.pt'))
 
-best_fc2_bias = torch.load(CHECKPOINT_DIR + '/best_fc2_bias.pt')
-best_fc2_weight = torch.load(CHECKPOINT_DIR + '/best_fc2_weight.pt')
+best_fc2_bias = torch.load(get_file_path(WAVE, 'best_fc2_bias.pt'))
+best_fc2_weight = torch.load(get_file_path(WAVE, 'best_fc2_weight.pt'))
 
-best_residual_bias = torch.load(CHECKPOINT_DIR + '/best_residual_bias.pt')
-best_residual_weight = torch.load(CHECKPOINT_DIR + '/best_residual_weight.pt')
+best_residual_bias = torch.load(get_file_path(WAVE, 'best_residual_bias.pt'))
+best_residual_weight = torch.load(get_file_path(WAVE, 'best_residual_weight.pt'))
 
-best_lstm_weight_ih0 = torch.load(CHECKPOINT_DIR + '/best_lstm_weight_ih0.pt')
-best_lstm_weight_hh0 = torch.load(CHECKPOINT_DIR + '/best_lstm_weight_hh0.pt')
-best_lstm_bias_ih0 = torch.load(CHECKPOINT_DIR + '/best_lstm_bias_ih0.pt')
-best_lstm_bias_hh0 = torch.load(CHECKPOINT_DIR + '/best_lstm_bias_hh0.pt')
+best_lstm_weight_ih0 = torch.load(get_file_path(WAVE, 'best_lstm_weight_ih0.pt'))
+best_lstm_weight_hh0 = torch.load(get_file_path(WAVE, 'best_lstm_weight_hh0.pt'))
+best_lstm_bias_ih0 = torch.load(get_file_path(WAVE, 'best_lstm_bias_ih0.pt'))
+best_lstm_bias_hh0 = torch.load(get_file_path(WAVE, 'best_lstm_bias_hh0.pt'))
 
 # columns
 dataframe_headers = list(read_dataframe('all').columns)[1:]
@@ -73,5 +79,5 @@ num_layers = 4
 output_dim = 1
 LAG = 15
 model = ScalingResidualLstmModel(input_dim, hidden_dim, num_layers, output_dim, LAG)
-model_path: str = CHECKPOINT_DIR + '/checkpoints/model_{:02d}.chk'.format(0)
+model_path: str = get_file_path(WAVE, 'checkpoints/model_{:02d}.chk'.format(0))
 model.load_state_dict(torch.load(model_path))
